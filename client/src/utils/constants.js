89deafe9c75ee6d122685419f770323272272ad8
Abhay Singh <abhaystudent2002@gmail.com>
2024-06-23 07:11:24 +0530

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0xe39d73bA3383f23601a26Aa1b4Bc4bdBB5485C69";
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,12 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+   
+ sepolia: {
+  url: 'https://eth-sepolia.g.alchemy.com/v2/1vo7O7bvXBfCkKrhHp-iywbodmj1oSfF',
+  accounts: ['ebc826db8260258829d4d3ab7bfd7fba378e48c34fbd00685f7187a56dee14a2'],
+},
+  },
+};
